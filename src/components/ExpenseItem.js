3598bac38ee18ceafea0877bf94c4775ec97759b
chebihiF <chebihi.f@ucd.ac.ma>
2022-06-14 19:45:32 +0100

    init commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+/** @format */
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>March 28th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+      </div>
+      <div>3000.59 Dh</div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
