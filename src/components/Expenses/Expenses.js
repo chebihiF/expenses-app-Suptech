a6c8a4e7b048eadd4f3c4b6cbf46e433fa49b07a
chebihiF <chebihi.f@ucd.ac.ma>
2022-06-21 20:28:19 +0100

    add date filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,28 @@
 /** @format */
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = ({ expenses }) => {
+  const [filteredYear, setFiltredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFiltredYear(selectedYear);
+  };
+
+  const filteredExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <>
       <Card className="expenses">
-        {expenses.map((expense) => (
+        <ExpensesFilter onChangeFilter={filterChangeHandler} />
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
