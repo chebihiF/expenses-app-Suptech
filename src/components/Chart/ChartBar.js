daf944ff26577e1abff18cba8069c5405266ccfd
chebihiF <chebihi.f@ucd.ac.ma>
2022-06-21 21:33:57 +0100

    chart v1

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,7 +1,21 @@
-const ChartBar = () => {
-    return (
-        
-    );
-}
+/** @format */
+
+const ChartBar = ({ value, maxValue }) => {
+  let barFillHeight = "0%";
+  if (maxValue > 0) {
+    barFillHeight = Math.round((value / max) * 100) + "%";
+  }
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label"></div>
+    </div>
+  );
+};
 
 export default ChartBar;
